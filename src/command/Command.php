7fcec7542ef9e9e68227feb8b7c11afa4689775c
ben <smoilbig@126.com>
2018-03-16 22:25:03 +0800

    config file.

--- src/command/Command.php
+++ src/command/Command.php
@@ -3,12 +3,13 @@
  * Created by PhpStorm.
  * User: ben
  * Date: 16/03/2018
- * Time: 17:48
+ * Time: 17:49
  */
+
+
 namespace src\command;
 
-class LoginCommand extends Command
+class Command
 {
-
 
 }
